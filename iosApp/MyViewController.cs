54d5c605804e972cee33e28ee6b795e0ad8b2094
Chris Risner <chrisner@microsoft.com>
2014-03-27 11:41:22 -0700

    Adding initial UI for ios

--- iosApp/MyViewController.cs
+++ iosApp/MyViewController.cs
@@ -7,10 +7,23 @@ namespace iosApp
 {
     public class MyViewController : UIViewController
     {
-        UIButton button;
+        UIButton mBtnClickMe;
+        UIButton mBtnSend;
+        UIButton mBtnLogin;
+        UIButton mBtnGetContacts;
+        UILabel mLblUsername;
+        UILabel mLblMessage;
+        UILabel mLblSendTo;
+        UITextField mTxtUsername;
+        UITextField mTxtMessage;
+        UITextField mTxtSendTo;
         int numClicks = 0;
         float buttonWidth = 200;
         float buttonHeight = 50;
+        float labelWidth = 100;
+        float labelHeight = 50;
+        float textWidth = 200;
+        float textHeight = 50;
 
         public MyViewController()
         {
@@ -24,27 +37,94 @@ namespace iosApp
             View.BackgroundColor = UIColor.White;
             View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
 
-            button = UIButton.FromType(UIButtonType.RoundedRect);
-
-            button.Frame = new RectangleF(
+            mBtnClickMe = UIButton.FromType(UIButtonType.RoundedRect);
+            mBtnSend = UIButton.FromType(UIButtonType.RoundedRect);
+            mBtnLogin = UIButton.FromType(UIButtonType.RoundedRect);
+            mBtnGetContacts = UIButton.FromType(UIButtonType.RoundedRect);
+            mBtnClickMe.Frame = new RectangleF(
                 View.Frame.Width / 2 - buttonWidth / 2,
-                View.Frame.Height / 2 - buttonHeight / 2,
+                10,
+                //View.Frame.Height / 2 - buttonHeight / 2,
+                buttonWidth,
+                buttonHeight);
+            mBtnSend.Frame = new RectangleF(
+                View.Frame.Width / 2 - buttonWidth / 2,
+                10 + buttonHeight + labelHeight *3,
+                buttonWidth,
+                buttonHeight);
+            mBtnLogin.Frame = new RectangleF(
+                View.Frame.Width / 2 - buttonWidth / 2,
+                10 + buttonHeight * 2 + labelHeight * 3,
+                buttonWidth,
+                buttonHeight);
+            mBtnGetContacts.Frame = new RectangleF(
+                View.Frame.Width / 2 - buttonWidth / 2,
+                10 + buttonHeight * 3 + labelHeight * 3,
                 buttonWidth,
                 buttonHeight);
 
-            button.SetTitle("Click me", UIControlState.Normal);
+            mLblUsername = new UILabel();
+            mLblMessage = new UILabel();
+            mLblSendTo = new UILabel();
+            mLblUsername.Frame = new RectangleF(20, 10 + buttonHeight, labelWidth, labelHeight);
+            mLblUsername.Text = "Username:";
+            mLblMessage.Frame = new RectangleF(20, 10 + buttonHeight + labelHeight, labelWidth, labelHeight);
+            mLblMessage.Text = "Message:";
+            mLblSendTo.Frame = new RectangleF(20, 10 + buttonHeight + labelHeight * 2, labelWidth, labelHeight);
+            mLblSendTo.Text = "Send To:";
 
-            button.TouchUpInside += (object sender, EventArgs e) =>
+            mTxtUsername = new UITextField();
+            mTxtMessage = new UITextField();
+            mTxtSendTo = new UITextField();
+            mTxtUsername.Frame = new RectangleF(20 + labelWidth, 10 + buttonHeight, textWidth, textHeight);
+            mTxtMessage.Frame = new RectangleF(20 + labelWidth, 10 + buttonHeight + textHeight, textWidth, textHeight);
+            mTxtSendTo.Frame = new RectangleF(20 + labelWidth, 10 + buttonHeight + textHeight * 2, textWidth, textHeight);
+            mTxtUsername.Text = "iOSUser";
+
+            mBtnClickMe.SetTitle("Click me", UIControlState.Normal);
+            mBtnSend.SetTitle("Send", UIControlState.Normal);
+            mBtnLogin.SetTitle("Login", UIControlState.Normal);
+            mBtnGetContacts.SetTitle("Get Contacts", UIControlState.Normal);
+
+            mTxtUsername.ShouldReturn = (textfield) =>
             {
-                button.SetTitle(String.Format("clicked {0} times", numClicks++), UIControlState.Normal);
+                textfield.ResignFirstResponder();
+                return true;
+            };
+            mTxtMessage.ShouldReturn = (textfield) =>
+            {
+                textfield.ResignFirstResponder();
+                return true;
+            };
+            mTxtSendTo.ShouldReturn = (textfield) =>
+            {
+                textfield.ResignFirstResponder();
+                return true;
+            };
+
+
+
+
+            mBtnClickMe.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                mBtnClickMe.SetTitle(String.Format("clicked {0} times", numClicks++), UIControlState.Normal);
                 //PCLProject.ServiceHelper.RecordClick();
                 ServiceHelper.GetInstance().RecordClick(numClicks, "ios");
             };
 
-            button.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleTopMargin |
+            mBtnClickMe.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleTopMargin |
                 UIViewAutoresizing.FlexibleBottomMargin;
 
-            View.AddSubview(button);
+            View.AddSubview(mBtnClickMe);
+            View.AddSubview(mLblUsername);
+            View.AddSubview(mLblMessage);
+            View.AddSubview(mLblSendTo);
+            View.AddSubview(mTxtUsername);
+            View.AddSubview(mTxtMessage);
+            View.AddSubview(mTxtSendTo);
+            View.AddSubview(mBtnSend);
+            View.AddSubview(mBtnLogin);
+            View.AddSubview(mBtnGetContacts);
         }
 
     }
